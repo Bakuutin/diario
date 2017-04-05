4741e1dffdbb86e47ee628441808a5e4077d4b5f
Igor Bakutin <igorbakutin@gmail.com>
2017-04-05 22:05:06 +0300

    Made primary name of a person unique

--- apps/persons/models.py
+++ apps/persons/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     text = models.TextField(default='', blank=True)
 
     def __str__(self):
