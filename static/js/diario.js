046ff9931c412dcef7decdc64ffa79a83af6bb45
kornilova <kornilova203@gmail.com>
2017-04-09 18:26:22 +0300

    infinite scroll and adding new days

--- static/js/diario.js
+++ static/js/diario.js
@@ -1,19 +1,22 @@
 const diario = angular.module('diario', []);
 
 diario.config(function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('((');
+    $interpolateProvider.startSymbol('((');  // django uses curvy brackets
     $interpolateProvider.endSymbol('))');
 });
 
 diario.directive('whenScrolled', ['$timeout', function () {
     return function (scope, elm, attr) {
-        const raw = elm[0];  // it is outer div with directive
+        const raw = elm[0];  // it is outer div which contains the directive
 
-        elm.bind('scroll', function () {
-            if (raw.scrollTop <= 100) { // load more entries before you hit the top
+        elm.bind('scroll', function () {  // when scroll to top
+            if (raw.scrollTop <= 100 && !scope.loading) { // load more entries before you hit the top
                 const sh = raw.scrollHeight;
-                scope.$apply(attr.whenScrolled);  // diarioly function loadMore()
-                raw.scrollTop = raw.scrollHeight - sh;
+                scope.$apply(attr.whenScrolled);  // apply function loadMore()
+                console.log("scroll - sh");
+                setTimeout(function () {  // wait till data is loaded. I don't know better way :(
+                    raw.scrollTop = raw.scrollHeight - sh;
+                }, 300);
             }
         });
     };
@@ -21,24 +24,66 @@ diario.directive('whenScrolled', ['$timeout', function () {
 
 diario.controller("Entries", function ($scope, $http, $timeout) {
     $scope.entries = [];
-    $scope.allEntries = [];
+    $scope.loading = 1;
+    $scope.next = '/api/days/?limit=15&reverse=true';
 
-    $http.get('/api/days').then((responce) => {
-        $scope.allEntries = responce.data;
-        $scope.loadMore();
+    $scope.scrollToBottom = function () {
         $timeout(function () {  // scroll to bottom
             const scroller = document.getElementById("fixed");
             scroller.scrollTop = scroller.scrollHeight;
         }, 0, false);
+    };
+
+    const calendar = new dhtmlXCalendarObject("calendar");
+    calendar.show();
+
+    calendar.attachEvent("onClick", function (date) {  // create new entry or scroll to existing
+        date = calendar.getFormatedDate("%Y-%m-%d", date);
+        $scope.$apply(function () {
+            $http.get('/api/days/' + date).then(function successCallback(response) {  // try to get day
+                console.log('day exists');
+
+            }, function errorCallback(response) {  // if date does not exist
+                $scope.createEntry(date);
+            });
+        });
     });
 
-    let counter = 0;
-    $scope.loadMore = function () {
-        for (let i = 0; i < 15; i++) {
-            $scope.entries.unshift($scope.allEntries[counter]);
-            counter += 1;
-        }
+    $scope.createEntry = function (date) {
+        console.log('create new day');
+        const newEntry = {
+            date: date,
+            title: "New Title",
+            text: "hello"
+        };
+        $scope.entries = [newEntry];
+        $.getJSON('/api/days/?limit=10&date_from=' + date, function(data) {
+            if (data.count !== 0) {
+                $scope.entries = $scope.entries.concat(data.results);
+            }
+        });
+        $.getJSON('/api/days/?limit=10&reverse=true&date_to=' + date, function(data) {
+            if (data.count !== 0) {
+                data.results.reverse();
+                $scope.entries = data.results.concat($scope.entries);
+            }
+        });
+        $http.post('/api/days/', newEntry);
     };
+
+    $scope.loadMore = function () {
+        $scope.loading = true;
+        $http.get($scope.next).then((responce) => {
+            data = responce.data;
+            $scope.next = data.next;
+            for (let i = 0; i < data.results.length; i++)
+                $scope.entries.unshift(data.results[i]);
+            $scope.loading = false;
+        });
+    };
+
+    $scope.loadMore();
+    $scope.scrollToBottom();
 });
 
 diario.directive("contenteditable", function ($http) {
@@ -57,7 +102,7 @@ diario.directive("contenteditable", function ($http) {
 
             element.bind("blur keyup change", function () {
                 scope.$apply(read);
-                $http.patch('/api/days/' + scope.entry.id + "/", {
+                $http.patch('/api/days/' + scope.entry.date + "/", {
                     text: scope.entry.text,
                     title: scope.entry.title
                 });
@@ -65,3 +110,4 @@ diario.directive("contenteditable", function ($http) {
         }
     };
 });
+
